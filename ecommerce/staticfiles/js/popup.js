f9ca4c4bffa2ad77e21d96abca87c8f9cf861a91
Rodrigo Joaquim <rjgalmeida@gmail.com>
2025-02-10 16:45:37 -0300

    Folder staticfiles  added to gitignore

--- ecommerce/staticfiles/js/popup.js
+++
@@ -1,6 +0,0 @@
-let elemFecharPopup = document.querySelector('.popup__fechar')
-let elemFecharPopupDiv = document.querySelector('.popup')
-
-elemFecharPopup.addEventListener('click', function(){
-	elemFecharPopupDiv.classList.add('popup--fechar')
-})
